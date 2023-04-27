c8370515cac22fe8d86dd83f0df648fd78539927
Lamisi11 <1deedeedolly@gmail.com>
2023-04-26 17:46:28 -0700

    Funtions and nested loops tasks 9-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - add two integers a and i and returns the result
+* @a: int to be added to i
+* @i: int to be added to a
+* Return: sum of a and i
+*/
+int add(int a, int i)
+{
+	int r;
+
+	r = a + i;
+	return (r);
+}
