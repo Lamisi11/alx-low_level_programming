7af0f2753bf6ee3dd89a8bd2d44ae35e954dec1e
Lamisi11 <1deedeedolly@gmail.com>
2023-04-27 19:00:49 -0700

    Functions and nested loops (more) tasks 0-6

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * @a: first int*b
+ * @b: second int*a
+ * Return: addition of the multiplicattion
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Tasks testing out the knowledge of functions and nested loops in alx
