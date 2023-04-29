b95af205b97aaa82a74a8d07849fe917c95917e2
Lamisi11 <1deedeedolly@gmail.com>
2023-04-28 17:13:16 -0700

    Debugging 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - Tests a function to see whether negetive or positive
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	i = 0;
+	negetive_or_positive(i);
+	return (0);
+}
