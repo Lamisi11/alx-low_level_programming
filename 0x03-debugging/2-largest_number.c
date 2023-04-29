6c736d9ec8ef74d1970a3d7da50ea7870b2fae80
Lamisi11 <1deedeedolly@gmail.com>
2023-04-28 17:47:06 -0700

    Debugging 2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the largest integer among 3 others
+* Return: 0
+*/
+
+int main(void)
+{
+	int i, j, k;
+	int largest;
+
+	i = 972;
+	j = -98;
+	k = 0;
+
+	largest = largest_number(i, j, k);
+
+	printf("%d is the largest number\n", largest);
+
+return (0);
+}
